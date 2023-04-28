5aa8edce3550f81b3378ebb691479d5f9a655857
BraisonCrece <braisoncrece@gmail.com>
2023-04-28 13:24:55 +0200

    [refactor] Change some styles on index

--- app/controllers/description_controller.rb
+++ app/controllers/description_controller.rb
@@ -1,12 +1,13 @@
-require 'httparty'
+require "httparty"
+
 class DescriptionController < ApplicationController
   def describe_dish
     plato = params[:plato]
-    description_type = params[:description_type] || 'plato'
+    description_type = params[:description_type] || "plato"
 
     prompts = {
-      plato: "Dado un prato de comida, describe brevemente o prato para unha carta dixital dun restaurante nun ton formal e adecuado e como se o escribise o cociñeiro do restaurante. Máximo 150 caracteres.\nExemplo: Prato: Cordeiro leital a baixa temperatura\nDescrición: Cordeiro leital cociñado a baixa temperatura, acompáñase con deliciosos espárragos verdes e puré de pataca.\nPrato: #{plato}\nDescrición:",
-      vino: "Dada a marca ou o nome dun viño, describe brevemente o viño para unha carta dixital dun restaurante nun ton formal e adecuado, coma se foses un experto enólogo e sommelier. Máximo 150 caracteres. En galego.\nExemplo: Viño: Casal de Armán\nDescrición: Combinamos uva de viñas con distintos tipos de chan e orientacións, exclusivamente no Val do Avia, para representar a tipicidade dos vinos desta zona  histórica ao norte da D.O. Ribeiro, para dar como resultado un viño de boa estructura e carácter graso, con aromas a nectarina, fiúncho, flores brancas, e un final persistente e elegantemente amargoso.\nViño: #{plato}\nDescrición:"
+      plato: "Dado un prato de comida, describe en Galego e brevemente o prato para unha carta dixital dun restaurante nun ton formal e adecuado e como se o escribise o cociñeiro do restaurante. Máximo 150 caracteres.\nExemplo: Prato: Cordeiro leital a baixa temperatura\nDescrición: Cordeiro leital cociñado a baixa temperatura, acompáñase con deliciosos espárragos verdes e puré de pataca.\nPrato: #{plato}\nDescrición:",
+      vino: "Dada a marca ou o nome dun viño, describe brevemente o viño para unha carta dixital dun restaurante nun ton formal e adecuado, coma se foses un experto enólogo e sommelier. Máximo 150 caracteres. En galego.\nExemplo: Viño: Casal de Armán\nDescrición: Viño de boa estructura e carácter graso, con aromas a nectarina, fiúncho, flores brancas, e un final persistente e elegantemente amargoso.\nViño: #{plato}\nDescrición:",
     }
 
     prompt = prompts[description_type.to_sym]
@@ -20,4 +21,3 @@ class DescriptionController < ApplicationController
     end
   end
 end
-
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,9 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  connect() {
-    console.log("hola");
-  }
 
   close() {
     const modal = document.querySelector('#product-modal')
