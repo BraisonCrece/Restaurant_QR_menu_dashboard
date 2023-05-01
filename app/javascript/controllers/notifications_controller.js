fe46abe589bf220b2a7b1373d0e7316ccdd18b6a
BraisonCrece <braisoncrece@gmail.com>
2023-05-01 16:46:21 +0200

    [change] notification animations

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -4,6 +4,27 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["x"]
   close(e) {
-    this.xTarget.hidden = true
+    this.hideNotification()
   }
+
+  connect() {
+    this.element.classList.add("animate__animated", "animate__fadeInRight");
+    this.timeout = setTimeout(() => {
+      this.hideNotification();
+    }, 3000);
+  }
+
+  disconnect() {
+    clearTimeout(this.timeout);
+  }
+
+  hideNotification() {
+    this.element.classList.remove("animate__fadeInRight");
+    this.element.classList.add("animate__fadeOutRight");
+
+    setTimeout(() => {
+      this.element.remove();
+    }, 1000); // Duración de la animación
+  }
+
 }
