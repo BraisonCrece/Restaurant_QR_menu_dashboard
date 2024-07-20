12c60cfc19edcaeca577eeca30883fd383567e74
BraisonCrece <104019638+BraisonCrece@users.noreply.github.com>
2024-07-20 19:59:38 +0200

    feat: improve UI consistency and enhance dark mode support
    - Reintroduced and adjusted .fixed-menu style in application.tailwind.css
    - Updated gradient colors for dark mode support in application.css
    - Improved skeleton class handling in modal_controller.js for consistency
     between dark and light modes
    - Enhanced form styles in various views by redefining max-width and margin
     classes
    - Adjusted mb-4 to mb-8 for form elements in several views for better spacing

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,39 +1,54 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["image", "skeleton"]
+  static targets = ["image", "skeleton"]
 
-    connect() {
-        this.loadImages().then(() => {
-            this.removeSkeletonClasses();
-        });
+  connect() {
+    const isDarkMode = document.documentElement.classList.contains("dark")
+    this.skeletonTargets.forEach(skeleton => {
+      if (isDarkMode) {
+        skeleton.classList.remove("skeleton")
+      } else {
+        skeleton.classList.remove("skeleton-dark")
+      }
+    })
+
+    this.loadImages().then(() => {
+      this.removeSkeletonClasses()
+    })
+  }
+
+  async loadImages() {
+    const imagePromises = this.imageTargets.map(image => {
+      return image.complete ? Promise.resolve() : this.waitForLoad(image)
+    })
+
+    await Promise.all(imagePromises)
+  }
+
+  waitForLoad(image) {
+    return new Promise(resolve => {
+      image.addEventListener("load", resolve, { once: true })
+    })
+  }
+
+  removeSkeletonClasses() {
+    this.skeletonTargets.forEach(skeleton =>
+      skeleton.classList.remove("skeleton", "skeleton-dark")
+    )
+  }
+
+  close(event) {
+    if (event.target === this.element) {
+      this.element.classList.remove("animate__fadeIn", "animate__animated")
+      this.element.classList.add(
+        "animate__fadeOut",
+        "animate__animated",
+        "animate__faster"
+      )
+      setTimeout(() => {
+        this.element.remove()
+      }, 500)
     }
-
-    async loadImages() {
-        const imagePromises = this.imageTargets.map(image => {
-            return image.complete ? Promise.resolve() : this.waitForLoad(image);
-        });
-
-        await Promise.all(imagePromises);
-    }
-
-    waitForLoad(image) {
-        return new Promise(resolve => {
-            image.addEventListener('load', resolve, { once: true });
-        });
-    }
-
-    removeSkeletonClasses() {
-        this.skeletonTargets.forEach(skeleton => skeleton.classList.remove('skeleton'));
-    }
-
-    close(event) {
-        if (event.target === this.element) {
-            this.element.classList.remove('animate__fadeIn', 'animate__animated')
-            this.element.classList.add('animate__fadeOut', 'animate__animated', 'animate__faster')
-            setTimeout(() => {
-                this.element.remove();
-            }, 500);
-        }
-    }
+  }
 }
