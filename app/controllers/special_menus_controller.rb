4fcebc3e0ab78b885ceeb5c96cf46394858bb3df
BraisonCrece <104019638+BraisonCrece@users.noreply.github.com>
2024-10-27 23:27:49 +0100

    feat(special_menus): add product management to special menus
    Added new functionality to manage products within special menus,
    including creating, editing, updating, and destroying products.
    Updated views to display product forms and lists within special
    menus. Also modified routing to support new product management
    actions. Improved code styling for better readability and
    maintainability.

--- app/controllers/special_menus_controller.rb
+++ app/controllers/special_menus_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Controller for the SpecialMenus model
 class SpecialMenusController < ApplicationController
   def index
     @special_menus = SpecialMenu.all
@@ -40,13 +43,81 @@ class SpecialMenusController < ApplicationController
     @special_menu.toggle(:active)
     @special_menu.save
     render turbo_stream: turbo_stream.replace("special_menu_active_#{@special_menu.id}",
-      partial: 'special_menus/active',
-      locals: { special_menu: @special_menu })
+                                              partial: 'special_menus/active',
+                                              locals: { special_menu: @special_menu })
+  end
+
+  def new_product
+    @product = Product.new
+    @special_menu = SpecialMenu.find(params[:special_menu_id])
+  end
+
+  def create_product
+    @product = Product.new(product_params)
+    @product.active = false
+    @product.lock_it!
+
+    Thread.new do
+      Translators::ProcessTranslationsService.new(@product, :create).call
+    end
+
+    @product.process_image(params[:product][:picture]) if params[:product][:picture]
+
+    if @product.save
+      redirect_to special_menus_path, notice: 'Plato engadido ao menú especial con éxito.'
+    else
+      render :new_product, alert: 'Erro ao engadir o plato ao menú especial.'
+    end
+  end
+
+  def edit_product
+    @product = Product.find(params[:product_id])
+    @special_menu = SpecialMenu.find(params[:special_menu_id])
+  end
+
+  def update_product
+    @product = Product.find(params[:product_id])
+
+    if @product.update(product_params)
+      if title_or_description_changed?
+        Thread.new do
+          Translators::ProcessTranslationsService.new(@product, :update).call
+        end
+      end
+
+      @product.process_image(params[:product][:picture]) if params[:product][:picture]
+
+      redirect_to special_menus_path, notice: 'Plato actualizado con éxito.'
+    else
+      render :edit_product, status: :unprocessable_entity, alert: 'Erro ao actualizar o plato.'
+    end
+  end
+
+  def destroy_product
+    @product = Product.find(params[:product_id])
+    @product.destroy
+
+    Thread.new do
+      Translators::ProcessTranslationsService.new(@product, :destroy).call
+    end
+
+    redirect_to special_menus_path, status: 303, notice: 'Plato eliminado con éxito.'
   end
 
   private
 
   def special_menu_params
     params.require(:special_menu).permit(:name, :description, :price)
+  end
+
+  def product_params
+    params.require(:product).permit(
+      :title, :description, :price, :picture, :special_menu_id,
+      :per_gram, :per_kilo, :per_unit, allergen_ids: []
+    )
+  end
+
+  def title_or_description_changed?
+    @product.previous_changes.include?('title') || @product.previous_changes.include?('description')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'users/sign_in', to: redirect('/') # trick in order to match the client QR code with the root path ;)
-  root "dynamic_router#call"
+  get 'users/sign_in', to: redirect('/')
+  root 'dynamic_router#call'
 
   devise_for :users, path: 'admin', path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }, controllers: {
     sessions: 'users/sessions',
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
   resources :wine_types
   resources :wine_origin_denominations, as: :denominations, path: 'denominations'
 
-  resources :settings, only: [:edit, :update]
+  resources :settings, only: %i[edit update]
 
   get '/control_panel', to: 'products#control_panel'
   get '/wines_control_panel', to: 'wines#control_panel', as: :wines_control_panel
@@ -28,4 +30,11 @@ Rails.application.routes.draw do
   get '/carta', to: 'products#index', as: :carta
   get '/pages_control', to: 'products#pages_control', as: :pages_control
   post '/reload_i18n', to: 'translate#reload_i18n'
+
+  # Special menu products
+  get '/special_menu/:special_menu_id/new_product', to: 'special_menus#new_product', as: :new_special_menu_product
+  post '/special_menu/:special_menu_id/create_product', to: 'special_menus#create_product', as: :create_special_menu_product
+  get '/special_menu/:special_menu_id/edit_product/:product_id', to: 'special_menus#edit_product', as: :edit_special_menu_product
+  patch '/special_menu/:special_menu_id/update_product/:product_id', to: 'special_menus#update_product', as: :update_special_menu_product
+  delete '/special_menu/:special_menu_id/destroy_product/:product_id', to: 'special_menus#destroy_product', as: :destroy_special_menu_product
 end
